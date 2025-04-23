619a9214462be3c4beac2684804a05c33329129a
jabedzaman <hi@jabed.dev>
2025-04-23 11:03:53 +0530

    fix: expose ports

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ import mysql.connector
 app = Flask(__name__)
 
 db = mysql.connector.connect(
-    host="db",
+    host="localhost",
     user="root",
     password="password",
     database="testdb"
